5ad73848a0d28bb597d59bef933232109ee023c9
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2023-07-05 02:08:02 +0530

    feat: get unread emails controller added

---
+++ controller/getUnreadEmail.js
@@ -0,0 +1,36 @@
+async function getUnreadEmails(gmail) {
+    try {
+        // Calculate the timestamp for 60 minutes ago
+        const lastHour = new Date();
+        lastHour.setMinutes(lastHour.getMinutes() - 60);
+        const timestamp = Math.floor(lastHour.getTime() / 1000);
+
+        const res = await gmail.users.messages.list({
+            userId: 'me',
+            q: `is:unread from:study12121@gmail.com after:${timestamp}`,
+        });
+
+        const emails = res.data.messages;
+        const emailIds = emails ? emails.map((email) => email.id) : [];
+
+        return emailIds;
+    } catch (err) {
+        console.error('Error retrieving emails:', err);
+        return [];
+    }
+}
+
+async function getLabelIds(gmail) {
+    try {
+      const response = await gmail.users.labels.list({ userId: 'me' });
+      const labels = response.data.labels;
+      const labelIds = labels.map((label) => label.id);
+      console.log('Label IDs:', labelIds);
+    } catch (error) {
+      console.error('Error retrieving label IDs:', error);
+    }
+  }
+
+module.exports = {
+    getUnreadEmails,
+};
